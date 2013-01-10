5975ae2007a8246181bd287033ca543b1487f8f1
dremok <macki.leander@gmail.com>
2013-01-10 14:31:20 +0100

    begins work on structure.py

---
+++ structure.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from token import *
+
+test = "I am thinking about buying the new iPhone. Looks really cooooool. Samsung Galaxy is not nearly as cool IMHO!"
+
+neg_str = r"""
+	(?:
+    	^(?:never|no|nothing|nowhere|noone|none|not|
+        	havent|hasnt|hadnt|cant|couldnt|shouldnt|
+        	wont|wouldnt|dont|doesnt|didnt|isnt|arent|aint
+    	)$
+	)
+	|
+	n't
+	"""
+
+neg_re = re.compile(neg_str, re.VERBOSE | re.I | re.UNICODE)
+clausePunct_re = re.compile(r"^[.:;!?]$", re.VERBOSE | re.I | re.UNICODE)
+
+class Structure:
+	def negate(self, words):
+		neg = False
+		for i in range(len(words)):
+			w = words[i]
+			if neg == False:
+				if neg_re.search(w):
+					neg = True
+			else:
+				if clausePunct_re.search(w):
+					neg = False
+				else:
+					words[i] = w + "_NEG"
+		return words
+
+if __name__ == '__main__':
+	tok = Tokenizer()
+	struc = Structure()
+	
+	words = tok.tokenize(test)
+	words = struc.negate(words)
+	print "\n".join(words)
--- token.py
+++ token.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Filename: token.py
 # -*- coding: utf-8 -*-
 
 import re
@@ -16,27 +17,36 @@ emoticon_string = r"""
       [<>]?
     )"""
 
+ellipsis_string = "(?:\.(?:\s*\.){1,})"
+
 re_strings = (
 	# Emoticons:
 	emoticon_string,
+    ellipsis_string,
 	# Remaining words:
 	r"""
-	(?:[a-z][a-z'\-_]+[a-z])			# Words with apastrophes or dashes.
-	|
-	(?:[\w_]+)							# Words without apastrophes or dashes.
-	|
-	(?:[+\-\$]?\d+[,/.:-]\d+[+\-%]?)	# Numbers.
-	|
-	(?:\.(?:\s*\.){1,})					# Ellipsis dots.
-	|
-	(?:\S)								# Everything else that isn't whitespace.
-	"""
-	)
+    (?:[a-z][a-z'\-_]+[a-z])                # Words with apostrophes or dashes.
+    |
+    (?:[+\-$%]?\d+[,/.:-]?\d+[+\-$%]?)  # Numbers, including fractions, decimals.
+    |
+    (?:[\w_]+)                              # Words without apostrophes or dashes.
+    |
+    (?:\S)                                  # Everything else that isn't whitespace.
+    """
+    )
 
 word_re = re.compile("|".join(re_strings), re.VERBOSE | re.I | re.UNICODE)
 emoticon_re = re.compile(emoticon_string, re.VERBOSE | re.I | re.UNICODE)
+ellipsis_re = re.compile(ellipsis_string, re.VERBOSE | re.I | re.UNICODE)
+allCaps_re = re.compile("^(?:[^a-z][^a-z]+$)", re.VERBOSE | re.UNICODE)
 
 class Tokenizer:
+    def to_lower(self, words):
+        return map((lambda x : x if (emoticon_re.search(x) or allCaps_re.search(x)) else x.lower()), words)
+
+    def norm_repetition(self, words):
+        return map((lambda x : re.sub(r"(.)\1{2,}", r"\1\1\1", x)), words)
+
     def tokenize(self, text):
     	# Try to ensure unicode:
         try:
@@ -45,10 +55,14 @@ class Tokenizer:
             text = str(text).encode('string_escape')
             text = unicode(text)
         words = re.findall(word_re, text)
-        words = map((lambda x : x if emoticon_re.search(x) else x.lower()), words)
+        words = self.to_lower(words)
+        words = self.norm_repetition(words)
+
         return words
 
 if __name__ == '__main__':
     tok = Tokenizer()
-    words = tok.tokenize("Listen y'all.... Is this text tokenized, or what? :D Btw, I owe my mom $10!")
+    words = tok.tokenize("Listen y'all.... Is this text tokenized, or what? :D BTW, precision can never be 100%. And I soooooooooo owe my CEO $10!")
     print "\n".join(words)
+
+# End of token.py
